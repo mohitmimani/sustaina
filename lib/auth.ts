345da85d0381c501c3b0851e9a9421e1c0c14bbf
Mohit Mimani <85110010+mohitmimani@users.noreply.github.com>
2025-03-11 15:47:41 +0530

    Implement authentication using better-auth library and add magic link support

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "@prisma/client";
+import { magicLink } from "better-auth/plugins";
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mongodb", // or "mysql", "mongodb", ...etc
+  }),
+
+  socialProviders: {
+    google: {
+      enabled: true,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [
+    magicLink({
+      sendMagicLink: async ({ email, token, url }, request) => {
+        console.log("Sending magic link to", email, "with token", token);
+        // send email to user
+        // We expect  a number to get in email will convert to a temp email
+        // like number@sustaina.com
+      },
+    }),
+  ],
+});
